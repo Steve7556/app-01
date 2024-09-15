fbdbf7d2b08bd3fc3eb66e360030da4e87fae376
Williams <williams@Williamss-MacBook-Pro.local>
2024-09-15 02:10:44 +0100

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Breadlandia exists in shawns head</Text>
+    <View className="items-center">
+      <Text className="font-pblack">Aora</Text>
     </View>
   );
 }
