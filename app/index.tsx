806a1e07ca16843deeee6dc173c3275068f99ac7
Steve7556 <stevechibs52@gmail.com>
2024-09-15 02:31:14 +0100

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
+import{ NativeWindStyleSheet } from "nativewind"
+
+NativeWindStyleSheet.setOutput({
+  default: "native"
+})
 
 export default function Index() {
   return (
-    <View className="items-center">
-      <Text className="font-pblack">Aora</Text>
+    <View className="items-center flex-1 justify-center bg-white">
+      <Text className="font-pblack text-3xl">Aora</Text>
     </View>
   );
 }
